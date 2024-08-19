9e51632831f5554d08a0fde0ff075c8fd22fa1b5
maoshan-classmate <452309154@qq.com>
2024-08-19 22:27:35 +0800

    注解信息完善

--- src/main/java/com/ms/aspect/MsLoggerAspect.java
+++ src/main/java/com/ms/aspect/MsLoggerAspect.java
@@ -19,11 +19,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.support.MultipartFilter;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author maoshan-classmate
@@ -38,22 +43,21 @@ public class MsLoggerAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MsLoggerAspect.class);
 
-    private static final ServletRequestAttributes REQUEST_ATTRIBUTES = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
 
     private static final TimeInterval TIMER = DateUtil.timer();
 
     private final SysLogger sysLogger = new SysLogger();
 
 
-
-    @Pointcut("@annotation(com.ms.annotation.MsLogger) ")
+    @Pointcut("@annotation(com.ms.annotation.MsLogger) || @within(com.ms.annotation.MsLogger) ")
     public void pointcut() {
     }
 
     @Before("pointcut()")
     public void doBefore(JoinPoint joinPoint) {
         if (msLoggerProperties.isEnable()) {
-            HttpServletRequest request = REQUEST_ATTRIBUTES.getRequest();
+            ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            HttpServletRequest request = requestAttributes.getRequest();
             sysLogger.setIpAddress(getClientIp(request));
             sysLogger.setApiUrl(request.getRequestURL().toString());
         }
@@ -63,14 +67,19 @@ public class MsLoggerAspect {
     public Object recordSysLogger(ProceedingJoinPoint joinPoint) throws Throwable {
         if (msLoggerProperties.isEnable()) {
             SysLogger sysLogger = buildSysLogger(joinPoint);
+            Object[] args = joinPoint.getArgs();
+            Object result = null;
             try {
                 TIMER.start();
-                Object proceed = joinPoint.proceed();
-                sysLogger.setCost(TIMER.intervalMs());
-                return proceed;
-            } finally {
-                LOGGER.info(JSONUtil.toJsonStr(sysLogger));
+                result = joinPoint.proceed(args);
+                long cost = TIMER.intervalMs();
+                sysLogger.setCost(cost);
+                printLog(sysLogger,joinPoint,JSONUtil.toJsonStr(result),cost);
+            } catch (Throwable e) {
+                LOGGER.error("记录日志异常：{}", e.getMessage());
+                throw e;
             }
+            return result;
         } else {
             return joinPoint.proceed();
         }
@@ -117,22 +126,70 @@ public class MsLoggerAspect {
      * @return IP地址
      */
     private String getClientIp(HttpServletRequest request) {
-        // 一般都会有代理转发，真实的ip会放在X-Forwarded-For
-        String xff = request.getHeader("X-Forwarded-For");
-        if (xff == null) {
-            return request.getRemoteAddr();
-        } else {
-            return xff.contains(",") ? xff.split(",")[0] : xff;
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
         }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        // 处理 X-Forwarded-For 头部可能出现的多个 IP 地址的情况
+        if (ip != null && ip.indexOf(",") > 0) {
+            ip = ip.split(",")[0];
+        }
+        return ip;
     }
 
     /**
-     * 获取匹配的Url
-     * @return url
+     * 过滤参数
+     * @param args 参数数组
+     * @return 过滤后的参数集合
      */
-//    private String getMatchUrl(){
-//        return msLoggerProperties.getServerPath();
-//    }
+    private List<Object> filterArgs(Object[] args) {
+        return Arrays.stream(args).filter(object -> !(object instanceof MultipartFilter)
+                && !(object instanceof HttpServletRequest)
+                && !(object instanceof HttpServletResponse)
+        ).collect(Collectors.toList());
+    }
+
+    /**
+     * 日志输出
+     * @param sysLogger 日志对象
+     * @param joinPoint 切点
+     * @param outParams 返回参数
+     * @param cost 花费时间
+     */
+    private void printLog(SysLogger sysLogger,JoinPoint joinPoint,String outParams,long cost) {
+        log.info("\n\r=======================================\n\r" +
+                        "日志描述:{} \n\r" +
+                        "请求地址:{} \n\r" +
+                        "请求接口URL:{} \n\r"+
+                        "请求方式:{} \n\r" +
+                        "请求类方法:{} \n\r" +
+                        "请求方法参数:{} \n\r" +
+                        "返回报文:{} \n\r" +
+                        "处理耗时:{} ms \n\r" +
+                        "=======================================\n\r",
+                sysLogger.getLogDesc(),
+                sysLogger.getIpAddress(),
+                sysLogger.getApiUrl(),
+                sysLogger.getMethodName(),
+                joinPoint.getSignature(),
+                JSONUtil.toJsonStr(filterArgs(joinPoint.getArgs())),
+                outParams,
+                cost
+        );
+    }
 
 
 }
