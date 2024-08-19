176aa90694f303084ea641ba233ad9ded891ec24
xuzhengwei <xuzhengwei@ci2c.com.cn>
2024-08-19 13:48:12 +0800

    切面定义和逻辑

--- src/main/java/com/ms/aspect/MsLoggerAspect.java
+++ src/main/java/com/ms/aspect/MsLoggerAspect.java
@@ -1,18 +1,25 @@
 package com.ms.aspect;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
 import cn.hutool.json.JSONUtil;
 import com.ms.annotation.MsLogger;
 import com.ms.dto.SysLogger;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.HashMap;
@@ -27,21 +34,35 @@ import java.util.HashMap;
 public class MsLoggerAspect {
 
 
-    private static final Logger logger = LoggerFactory.getLogger(MsLoggerAspect.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MsLoggerAspect.class);
 
-//    private static final TimeInterval timer = DateUtil.timer();
+    private static final TimeInterval TIMER = DateUtil.timer();
+
+    private final SysLogger sysLogger = new SysLogger();
 
     @Pointcut(value = "@annotation(com.ms.annotation.MsLogger)")
     public void pointcut() {
+    }
+
+    @Before("pointcut()")
+    public void doBefore(JoinPoint joinPoint){
+        // 通过Spring提供的请求上下文工具，获取request
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        sysLogger.setIpAddress(getClientIp(request));
+        sysLogger.setApiUrl(request.getRequestURL().toString());
     }
 
     @Around("pointcut()")
     public Object recordSysLogger(ProceedingJoinPoint joinPoint) throws Throwable {
         SysLogger sysLogger = buildSysLogger(joinPoint);
         try {
-             return joinPoint.proceed();
+            TIMER.start();
+            Object proceed = joinPoint.proceed();
+            sysLogger.setCost(TIMER.intervalMs());
+            return proceed;
         } finally {
-            logger.info(JSONUtil.toJsonStr(sysLogger));
+            LOGGER.info(JSONUtil.toJsonStr(sysLogger));
         }
     }
 
@@ -49,8 +70,6 @@ public class MsLoggerAspect {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Method method = methodSignature.getMethod();
         MsLogger msLogger = method.getAnnotation(MsLogger.class);
-        SysLogger sysLogger = new SysLogger();
-        sysLogger.setIpAddress("127.0.0.1");
         sysLogger.setLogDesc(msLogger.desc());
         sysLogger.setMethodName(method.getDeclaringClass().getSimpleName() + "." + method.getName());
         try {
@@ -69,8 +88,23 @@ public class MsLoggerAspect {
             sysLogger.setParams(JSONUtil.toJsonStr(paramMap));
             return sysLogger;
         } catch (Exception e) {
-            logger.error("构建入参异常：{}", e.getMessage());
+            LOGGER.error("构建入参异常：{}", e.getMessage());
         }
         return sysLogger;
+    }
+
+    /**
+     * 获取IP地址
+     * @param request 请求
+     * @return IP地址
+     */
+    private String getClientIp(HttpServletRequest request) {
+        // 一般都会有代理转发，真实的ip会放在X-Forwarded-For
+        String xff = request.getHeader("X-Forwarded-For");
+        if (xff == null) {
+            return request.getRemoteAddr();
+        } else {
+            return xff.contains(",") ? xff.split(",")[0] : xff;
+        }
     }
 }
